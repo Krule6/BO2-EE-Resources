07f01fd0540f71939f14b26f3ab8d8692a71fe46
Krule6 <140613194+Krule6@users.noreply.github.com>
2025-11-07 04:27:10 +0100

    Remove onReady function for body class addition
    Removed the self-invoking function that adds 'ready' class to the body.

--- Die Rise/Die Rise/script.js
+++ Die Rise/Die Rise/script.js
@@ -69,12 +69,3 @@ window.addEventListener("DOMContentLoaded", () => {
 
 });
 
-(function () {
-    function onReady() { document.body.classList.add('ready'); }
-    if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', onReady);
-    } else {
-        onReady();
-    }
-})();
-
