2583d68c4b0b61349ca8caa143d09dc393f574f8
Krule6 <140613194+Krule6@users.noreply.github.com>
2025-11-07 04:38:40 +0100

    Implement document ready state handler
    Add a function to handle document ready state.

--- Die Rise/Die Rise/main.js
+++ Die Rise/Die Rise/main.js
@@ -69,3 +69,12 @@ window.addEventListener("DOMContentLoaded", () => {
 
 });
 
+(function () {
+    function onReady() { document.body.classList.add('ready'); }
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', onReady);
+    } else {
+        onReady();
+    }
+})();
+
